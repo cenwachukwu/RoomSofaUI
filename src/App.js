28195fb719d825cc9e12ff32463c084076e3dfc2
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-04-10 12:42:08 -0400

    created app js page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from '@reach/router';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div className="App">
+        <Router>
+          <Home path="/" />
+        </Router>
+      </div>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
