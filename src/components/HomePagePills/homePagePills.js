dee7d8b8895c6baa5c63a52ba7e83feed9926d99
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-04-10 14:33:05 -0400

    created pills

--- src/components/HomePagePills/homePagePills.js
+++ src/components/HomePagePills/homePagePills.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Pills.scss';
 import Pills from '../Pills/Pills';
+import chair from '../../utils/homepills/chair.jpg';
+import loveseat from '../../utils/homepills/loveseat.jpg';
+import recliner from '../../utils/homepills/recliner.jpg';
+import sectional from '../../utils/homepills/sectional.jpg';
+import sofa from '../../utils/homepills/sofa.jpg';
 
 const PillBox = () => {
   return (
     <div className="PillBox">
       <div className="boxPills">
-        <Pills categoryName={'Sofa'} />
-        <Pills categoryName={'Love Seat'} />
-        <Pills categoryName={'Chair'} />
-        <Pills categoryName={'Recliners'} />
-        <Pills categoryName={'Sectionals'} />
+        <Pills props={sofa} categoryName={'Sofa'} />
+        <Pills props={loveseat} categoryName={'Love Seat'} />
+        <Pills props={chair} categoryName={'Chair'} />
+        <Pills props={recliner} categoryName={'Recliners'} />
+        <Pills props={sectional} categoryName={'Sectionals'} />
+
         <div className="extraPill">
           <div>
             <a href="#">Shop all Room Sofa deals</a>
